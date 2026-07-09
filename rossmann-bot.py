e145b580eb98e4ba20c5c53191907acd8205d2fe
Marina Onohara <marina.onohar@gmail.com>
2026-07-09 00:09:39 -0300

    Atualização rossmann-bot

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -5,6 +5,7 @@ import pandas as pd
 import requests
 from flask import Flask , request, Response 
 
+
 # constants
 TOKEN = '8913649606:AAFPrM9XqDy6hNSDl3RcnltxANsXV1Q98t0'
 
@@ -117,3 +118,45 @@ def index():
 if __name__ =='__main__':
     port = os.environ.get('PORT', 5000)
     app.run(host= '0.0.0.0', port = port)
+
+def load_dataset(store_id):
+    # loading test dataset
+    df10 = pd.read_csv( '../Dataset/test.csv')
+    df_store_raw = pd.read_csv('../Dataset/store.csv')
+
+    # merge test dataset + store
+    df_test = pd.merge(df10, df_store_raw, how='left', on='Store')
+
+    # choose store for prediction
+    df_test = df_test[df_test['Store']== store_id]
+
+    # remove closed days
+    df_test = df_test[df_test['Open'] != 0]
+    df_test = df_test[~df_test['Open'].isnull()]
+    df_test = df_test.drop( 'Id', axis=1 )
+
+    # convert Dataframe to json
+    data = json.dumps( df_test.to_dict( orient='records'))
+
+    return data
+
+def predict(data):
+    # API Call
+    url = 'https://rossmann-hander-api.onrender.com'
+    header = {'Content-type': 'application/json' }
+    data = data
+
+    r = requests.post( url, data=data, headers=header)
+    print('Status Code {}'.format(r.status_code))
+
+    d1 = pd.DataFrame( r.json(), columns=r.json()[0].keys() )
+
+    return d1
+
+#d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
+#
+#for i in range (len(d2)):
+#    print ('Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(
+#        d2.loc[i, 'store'],
+#        d2.loc[i, 'prediction'] ) )
+
